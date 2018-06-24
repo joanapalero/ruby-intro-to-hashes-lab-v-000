25a9b2191b8f8014763cad19780466bfee3f6fcb
Joana Palero <joana.palero24@gmail.com>
2018-06-24 16:23:06 +0000

    automatically backed up by learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -18,11 +18,10 @@ end
 def monopoly_with_second_tier
 	monopoly = {
 	  :railroads => {
-	    :pieces => 4
-	  },
-	  :names => {
-	    
+	    :pieces => 4,
+	    :names => {}
 	  }
+	  
      
 	  
 	}
